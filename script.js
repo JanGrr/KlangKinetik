d63470eabe6cafe9c36cba74e3285acb662dd0fb
JanGrr <jangr.ger@web.de>
2023-11-24 18:43:59 +0100

    Kein Ton Fix

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ document.addEventListener('DOMContentLoaded', function () {
         accelerationY.textContent = acceleration.y.toFixed(2);
         accelerationZ.textContent = acceleration.z.toFixed(2);
 
-        var totalAccelerationMath = Math.abs(acceleration.x) + Math.abs(acceleration.y) + Math.abs(acceleration.z);
-        totalAcceleration.textContent = totalAccelerationMath.toFixed(2);
+        var playbackSpeed = Math.abs(acceleration.x) + Math.abs(acceleration.y) + Math.abs(acceleration.z);
+        totalAcceleration.textContent = playbackSpeed.toFixed(2);
 
         if (playbackSpeed > 0.4) {
-            audioPlayer.playbackRate = totalAccelerationMath;
+            audioPlayer.playbackRate = playbackSpeed;
         }
         
         var volume = Math.min(1, playbackSpeed);
         audioPlayer.volume = volume;
 
-        console.log("Total Acceleration: " + totalAccelerationMath);
+        console.log("Total Acceleration: " + playbackSpeed);
         console.log("Playback Speed: " + playbackSpeed);
 
         if (audioPlayer.paused) {
-            if (totalAccelerationMath > 1) {
+            if (playbackSpeed > 1) {
                 console.log("Starting playback.");
                 audioPlayer.play();
             }
