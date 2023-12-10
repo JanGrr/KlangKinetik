2ee6a270e98674153a700d49ff411cdfc016a682
JanGrr <jangr.ger@web.de>
2023-12-10 19:49:15 +0100

    Neustart

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var audioPlayer = document.getElementById('audioPlayer');
-    var accelerationX = document.getElementById('accelerationX');
-    var accelerationY = document.getElementById('accelerationY');
-    var accelerationZ = document.getElementById('accelerationZ');
-    var totalAcceleration = document.getElementById('totalAcceleration');
-    var start = false;
+if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrientation, false);
+}
 
-    window.addEventListener('devicemotion', handleMotion);
+function handleOrientation(event) {
+    var alpha = event.alpha; // Drehrate um die Z-Achse
+    var beta = event.beta;   // Neigung um die X-Achse
+    var gamma = event.gamma; // Neigung um die Y-Achse
 
-    function handleMotion(event) {
-        var acceleration = event.acceleration;
-
-        // Zeige die Beschleunigungswerte an
-        accelerationX.textContent = acceleration.x.toFixed(2);
-        accelerationY.textContent = acceleration.y.toFixed(2);
-        accelerationZ.textContent = acceleration.z.toFixed(2);
-
-        var playbackSpeed = Math.abs(acceleration.x) + Math.abs(acceleration.y) + Math.abs(acceleration.z);
-        //console.log("playbackSpeed 1: " + playbackSpeed);
-        playbackSpeed /= 5;
-        totalAcceleration.textContent = playbackSpeed.toFixed(2);
-
-        if(!start) {
-            if (playbackSpeed > 1) {
-                start = true;
-                audioPlayer.play();
-            }
-        } else if (playbackSpeed > 0.2) {
-            audioPlayer.play();
-            audioPlayer.playbackRate = playbackSpeed;
-            var volume = Math.min(1, playbackSpeed);
-            audioPlayer.volume = volume;
-        } else {
-            audioPlayer.pause();
-        }
-    }
-});
+    // Aktualisiere die Werte auf der Website
+    document.getElementById('alphaValue').innerText = alpha.toFixed(2);
+    document.getElementById('betaValue').innerText = beta.toFixed(2);
+    document.getElementById('gammaValue').innerText = gamma.toFixed(2);
+}
