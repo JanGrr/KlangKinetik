85b0c604644593bbf55ec4529f020b4de0cd8786
JanGrr <jangr.ger@web.de>
2024-01-06 17:54:37 +0100

    4th Try at 360 Sound

--- js/script_klangkinetik_audio.js
+++ js/script_klangkinetik_audio.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if(isMusicPlaying) {
             panvalue = (alpha-180)/180 // Bereich: -1 bis 1
             if (panvalue >= -0.5 && panvalue <= 0.5) {
-                panvalue * 2;
+                panvalue *= 2;
             } else {
                 if (panvalue >= 0) {
                     panvalue = (-panvalue + 1) * 2
