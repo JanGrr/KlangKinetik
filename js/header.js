2e62929de6fbcf40b85377992eb83e4ad93eed09
JanGrr <jangr.ger@web.de>
2024-01-09 19:47:11 +0100

    some more styling

--- js/header.js
+++ js/header.js
@@ -1,13 +1,26 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <p>
-                <a href="../index.html">Homepage</a>
-                <a href="wiki.html">Wiki</a>
-                <a href="klangkinetik.html">KlangKinetik</a>
-            </p>
+            <header>
+                <a class="logo" href="../index.html">KlangKinetik</a>
+                <nav>
+                    <ul class="nav-links">
+                        <li><a href="../index.html">Homepage</a></li>
+                        <li><a href="klangkinetik.html">Player</a></li>
+                    </ul>
+                </nav>
+                <a href="wiki.html"><button>Wiki</button></a>
+            </header>
         `
     }
 }
 
 customElements.define('header-container', Header)
+
+/*
+            <p>
+                <a href="../index.html">Homepage</a>
+                <a href="wiki.html">Wiki</a>
+                <a href="klangkinetik.html">KlangKinetik</a>
+            </p>
+*/
