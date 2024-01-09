4875897fd1454713ae85dbd490b857b3cb1a47ea
JanGrr <jangr.ger@web.de>
2024-01-09 21:26:00 +0100

    first try to fix mobile device height problem

--- js/script_index.js
+++ js/script_index.js
@@ -1,3 +1,6 @@
 window.addEventListener("orientationchange", function() {
     location.reload();
 });
+
+var mainContainer = document.querySelector('.main-container');
+mainContainer.style.height = window.innerHeight;
