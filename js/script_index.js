e7527521d3cdd6fcf0cf94ee0f486a9bcab41102
JanGrr <jangr.ger@web.de>
2024-01-09 21:38:59 +0100

    2nd try to fix viewport problem for mobile devices

--- js/script_index.js
+++ js/script_index.js
@@ -1,6 +1,3 @@
 window.addEventListener("orientationchange", function() {
     location.reload();
 });
-
-var mainContainer = document.querySelector('.main-container');
-mainContainer.style.height = window.innerHeight;
